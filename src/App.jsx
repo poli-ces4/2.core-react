9456973ce5195f3481013b8af015ba5f041bd2b1
Giovanny Vargas <yovaz05@gmail.com>
2023-09-19 19:21:15 -0500

    clase 19-09-2023

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 import VideoList from "./components/VideoList";
 import { playListMusica, playListProgramacion } from "./data/data";
 import style from "./App.module.css";
+import FormControlado from "./components/FormControlado";
+import LifeCycle from "./components/LifeCycle";
+import { useState } from "react";
 
 const App = () => {
+  const [showLifeCycle, setShowLifeCycle] = useState(false);
+
   return (
     <div className={style.container}>
       <VideoList title="Programacion" playList={playListProgramacion} />
       <VideoList title="Musica" playList={playListMusica} />
+      <FormControlado />
+      {showLifeCycle && <LifeCycle />}
+      <button onClick={() => setShowLifeCycle(!showLifeCycle)}>
+        {showLifeCycle ? "Ocultar" : "Mostrar"}
+      </button>
     </div>
   );
 };
---
+++ src/components/LifeCycle.jsx
@@ -0,0 +1,39 @@
+import { useEffect, useState } from "react";
+console.log("Pre-Render");
+const LifeCycle = () => {
+  console.log("Logic-Render");
+  const [counter1, setCounter1] = useState(0);
+  const [counter2, setCounter2] = useState(0);
+
+  useEffect(() => {
+    console.log("useEffect default");
+    return () => {
+      console.log("Componente desmontado");
+    };
+  });
+
+  useEffect(() => {
+    console.log("useEffect sin dependencias []");
+  }, []);
+
+  useEffect(() => {
+    console.log("useEffect con dependencias [counter1]");
+  }, [counter1]);
+
+  useEffect(() => {
+    console.log("useEffect con dependencias [counter1,counter2]");
+  }, [counter1, counter2]);
+
+  return (
+    <div>
+      {console.log("Return-Render")}
+      <h2>LifeCycle</h2>
+      <h4>Counter 1:</h4>
+      <h4>Counter 2:</h4>
+      <button onClick={() => setCounter1(counter1 + 1)}>Counter 1+</button>
+      <button onClick={() => setCounter2(counter2 + 1)}>Counter 2+</button>
+    </div>
+  );
+};
+
+export default LifeCycle;
